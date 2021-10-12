bf37f6d224a0a9361a7c0c689b8f6dca80794b98
Kolpsi <tykataf@gmail.com>
2021-10-12 21:21:12 +0300

    Третье задане

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "0");
+const a= prompt("Один из последних просмотренных фильмов?"),
+      b = prompt("На сколько оцените его?"),
+      c = prompt("Один из последних просмотренных фильмов?"),
+      d = prompt("На сколько оцените его?");
+
+
 
 const personalMovieDB = {
-    count = numberOfFilms,
-    movies = {},
-    actors = {},
-    genres = [],
-    privat = false
+    count : numberOfFilms,
+    movies : {
+    },
+    actors : {},
+    genres : [],
+    privat : false
 };
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
