9c87de69b9ad872344f43dd54d13c161b3bcb668
Kolpsi <tykataf@gmail.com>
2021-10-12 20:53:34 +0300

    Второе задание

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "0");
 
+const personalMovieDB = {
+    count = numberOfFilms,
+    movies = {},
+    actors = {},
+    genres = [],
+    privat = false
+};
