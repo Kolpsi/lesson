0f43caef3d159b9481fd00b70c8acf0115ec43a9
Kolpsi <tykataf@gmail.com>
2021-10-13 20:37:54 +0300

    Циклы и условия

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "0");
-const a= prompt("Один из последних просмотренных фильмов?"),
-      b = prompt("На сколько оцените его?"),
-      c = prompt("Один из последних просмотренных фильмов?"),
-      d = prompt("На сколько оцените его?");
-
-
 
 const personalMovieDB = {
     count : numberOfFilms,
@@ -15,5 +9,26 @@ const personalMovieDB = {
     privat : false
 };
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+
+for (i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?"),
+          b = prompt("На сколько оцените его?");
+
+    if (a != null && b != null & a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+};
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов")
+} if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель")
+} if (personalMovieDB.count > 30) {
+    console.log('Вы кионман')
+};
+
