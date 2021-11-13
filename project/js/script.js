dca76f08b63dca31dee15a9335f5b953b06f857e
rebar lenovo <tykataf@gmail.com>
2021-11-13 20:46:05 +0300

    Приступил к33 заданию

--- project/js/script.js
+++ project/js/script.js
@@ -28,16 +28,51 @@ const adv = document.querySelectorAll('.promo__adv img'),
       poster = document.querySelector('.promo__bg'),
       promoGenre = poster.querySelector('.promo__genre'),
       promoBg = document.querySelector('.promo__bg'),
-      films = document.querySelectorAll('.promo__interactive-item');
+      movieList = document.querySelector('.promo__interactive-list'),
+      films = document.querySelectorAll('.promo__interactive-item'),
+      form = document.querySelector('.add'),
+      btn = form.querySelector('button'),
+      input = form.querySelector('.adding__input'),
+      del = movieList.querySelectorAll('.delete');
+
 
 promoBg.style.background = 'url("../project/img/bg.jpg") center center/cover no-repeat';
 promoGenre.textContent = 'Драма';
 movieDB.movies.sort();
 
-films.forEach((item, i) => {
-  item.innerHTML = `${i+1} ` + movieDB.movies[i];
+movieList.innerHTML = '';
+movieDB.movies.forEach((item, i) => {
+  movieList.innerHTML += `<li class="promo__interactive-item">${i + 1} ${item}
+  <div class="delete"></div>
+</li>`;
 });
+
+let deleteElement = function(e) {
+    console.log(e.target);
+}
+
+del.forEach(btn => {
+    btn.addEventListener('click', deleteElement);
+});
+
+
 
 adv.forEach(item => {
     item.remove();
+});
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();   
+    if (input.value.length < 21) {
+        movieDB.movies.push(input.value);
+    } else {
+        movieDB.movies.push(input.value.slice(0, 22) + '...');
+    }
+    movieDB.movies.sort();
+    movieList.innerHTML = '';
+    movieDB.movies.forEach((item, i) => {
+        movieList.innerHTML += `<li class="promo__interactive-item">${i + 1} ${item}
+        <div class="delete"></div>
+    </li>`;
+    });
 });
